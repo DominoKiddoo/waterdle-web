d9970e008a891c136f3ca62f35d4f69a040a0e1c
DominoKiddoo <lucas.ellul@gmail.com>
2026-07-02 20:10:18 +1000

    viewport?

--- Waterdle.service.worker.js
+++ Waterdle.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782986841|2883687561';
+const CACHE_VERSION = '1782986998|3040620402';
 /** @type {string} */
 const CACHE_PREFIX = 'Waterdle-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
